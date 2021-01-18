5a399a60478ca67469bcd6f552ae25def687e3ce
stormbringer8 <stormbringer@programmer.net>
2021-01-17 21:27:15 -0600

    spring initializer

---
+++ src/main/java/pub/ayq/AyqApplication.java
@@ -0,0 +1,13 @@
+package pub.ayq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pub/ayq/AyqApplicationTests.java
@@ -0,0 +1,13 @@
+package pub.ayq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
